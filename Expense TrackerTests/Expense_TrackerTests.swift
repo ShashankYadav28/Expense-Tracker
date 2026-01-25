c0ca991c09d9cc7c15e34118060e5e51a2732777
Shashank Yadav <ShashankYadav2803@gmail.com>
2026-01-25 22:45:36 +0530

    Initial Commit

---
+++ Expense TrackerTests/Expense_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerTests.swift
+//  Expense TrackerTests
+//
+//  Created by Shashank Yadav on 25/01/26.
+//
+
+import Testing
+@testable import Expense_Tracker
+
+struct Expense_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
